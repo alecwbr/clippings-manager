2dead4f5c9602b8091cc1d5f504fc0e1259c7b4b
alecwbr <110862427+alecwbr@users.noreply.github.com>
2022-08-31 22:30:43 -0600

    remove commented/unused line

--- vue-app/vite.config.js
+++ vue-app/vite.config.js
@@ -17,7 +17,6 @@ export default defineConfig({
         target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
-        // rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   },
