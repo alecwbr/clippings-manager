ed3a77c91b6a9c6d25390ed238d4d2ce28f07acc
alecwbr <110862427+alecwbr@users.noreply.github.com>
2022-08-31 21:12:49 -0600

    add more unit tests

--- server/tests/unit/test_clippings_parser.py
+++ server/tests/unit/test_clippings_parser.py
@@ -3,22 +3,57 @@ from datetime import datetime
 from app import db
 from unittest import mock
 import pytest
-from clippings_parser import ClippingsParser, ParsingError
-
-def test_clippings_parser(mock_file_handle):
-    with mock_file_handle:
-        parser = ClippingsParser(mock_file_handle)
-        clips = parser.get_clips()
-        assert clips is not None
-        assert clips[0].book_title == 'Test Book'
-        assert clips[0].author == 'Fake Author'
-        assert clips[0].clip_type == 'Highlight'
-        assert clips[0].location == '1337'
-        assert clips[0].date == datetime(2022, 8, 20, 2, 5)
-        assert clips[0].highlight == 'This is a test highlight'
+from clippings_parser import ClippingsParser, ParsedClip, ParsingError
 
 def test_clippings_parser_parsing_error(bad_mock_file_handle):
     with bad_mock_file_handle:
         parser = ClippingsParser(bad_mock_file_handle)
         with pytest.raises(ParsingError):
             parser.get_clips()
+
+def test_clippings_parser_get_clips_returns_type_ParsedClip(mock_file_handle):
+    with mock_file_handle:
+        parser = ClippingsParser(mock_file_handle)
+        assert parser.get_clips() is not None
+
+def test_clippings_parser_is_correct_size(mock_file_handle):
+    with mock_file_handle:
+        parser = ClippingsParser(mock_file_handle)
+        clips = parser.get_clips()
+        assert len(clips) == 2
+
+def test_clippings_parser_book_title(mock_file_handle):
+    with mock_file_handle:
+        parser = ClippingsParser(mock_file_handle)
+        clips = parser.get_clips()
+        assert clips[0].book_title == 'Test Book'
+
+def test_clippings_parser_author(mock_file_handle):
+    with mock_file_handle:
+        parser = ClippingsParser(mock_file_handle)
+        clips = parser.get_clips()
+        assert clips[0].author == 'Fake Author'
+
+def test_clippings_parser_clip_type(mock_file_handle):
+    with mock_file_handle:
+        parser = ClippingsParser(mock_file_handle)
+        clips = parser.get_clips()
+        assert clips[0].clip_type == 'Highlight'
+
+def test_clippings_parser_location(mock_file_handle):
+    with mock_file_handle:
+        parser = ClippingsParser(mock_file_handle)
+        clips = parser.get_clips()
+        assert clips[0].location == '1337'
+
+def test_clippings_parser_date(mock_file_handle):
+    with mock_file_handle:
+        parser = ClippingsParser(mock_file_handle)
+        clips = parser.get_clips()
+        assert clips[0].date == datetime(2022, 8, 20, 2, 5)
+
+def test_clippings_parser_book_title(mock_file_handle):
+    with mock_file_handle:
+        parser = ClippingsParser(mock_file_handle)
+        clips = parser.get_clips()
+        assert clips[0].highlight == 'This is a test highlight'
