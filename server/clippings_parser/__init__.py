463842955b69b74fd369c5cb6b525273ea47a1b6
alecwbr <110862427+alecwbr@users.noreply.github.com>
2022-09-04 02:47:17 -0600

    refactor parser make private methods have one underscore and add type hinting

--- server/clippings_parser/__init__.py
+++ server/clippings_parser/__init__.py
@@ -24,16 +24,16 @@ class ClippingsParser():
         self.__my_clippings_file = my_clippings_file
         self.parsed_clips = []
 
-    def __parse_clip_title(self, clip):
+    def _parse_clip_title(self, clip: ParsedClip) -> str:
         self.__clean_title_author = clip[0].lstrip("\ufeff").rstrip()
         clip_title = self.__clean_title_author.split('(')[0].rstrip()
         return clip_title
 
-    def __parse_clip_author(self, clip):
+    def _parse_clip_author(self, clip: ParsedClip) -> str:
         author = re.findall(r"\(([^)]+)\)$", self.__clean_title_author)[0]
         return author
 
-    def __parse_clip_type(self, clip):
+    def _parse_clip_type(self, clip: ParsedClip) -> str:
         if 'Bookmark' in clip[1]:
             return 'Bookmark'
         elif 'Highlight' in clip[1]:
@@ -43,7 +43,7 @@ class ClippingsParser():
         else:
             return None
 
-    def __parse_clip_date(self, clip):
+    def _parse_clip_date(self, clip: ParsedClip) -> datetime:
         sub_str = '| Added on '
         sub_str_len = len(sub_str)
         clip_len = len(clip[1])
@@ -52,7 +52,7 @@ class ClippingsParser():
         dt = datetime.strptime(date, '%A, %B %d, %Y %I:%M:%S %p')
         return dt.astimezone()
 
-    def __parse_clip_location(self, clip):
+    def _parse_clip_location(self, clip: ParsedClip) -> str:
         sub_str = 'Location '
         sub_str_len = len(sub_str)
         added_on_index = clip[1].find(' | Added on ')
@@ -60,7 +60,7 @@ class ClippingsParser():
         location = clip[1][on_location_index + sub_str_len : added_on_index]
         return location
 
-    def __parse_clip_highlight(self, clip):
+    def _parse_clip_highlight(self, clip: ParsedClip) -> str:
         return clip[3].rstrip() 
 
     def get_clips(self) -> List[ParsedClip]:
@@ -78,12 +78,12 @@ class ClippingsParser():
                     book_clips.append(list(group))
 
         for clip in book_clips:
-            title = self.__parse_clip_title(clip)
-            author = self.__parse_clip_author(clip)
-            clip_type = self.__parse_clip_type(clip)
-            date = self.__parse_clip_date(clip)
-            location = self.__parse_clip_location(clip)
-            highlight = self.__parse_clip_highlight(clip)
+            title = self._parse_clip_title(clip)
+            author = self._parse_clip_author(clip)
+            clip_type = self._parse_clip_type(clip)
+            date = self._parse_clip_date(clip)
+            location = self._parse_clip_location(clip)
+            highlight = self._parse_clip_highlight(clip)
 
             parsed_clip = ParsedClip(book_title=title, author=author, clip_type=clip_type, date=date, location=location, highlight=highlight)
             self.parsed_clips.append(parsed_clip)
