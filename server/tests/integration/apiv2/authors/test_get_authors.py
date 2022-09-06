db06abe4f9fea7297c27f2ecad07ca1b8f69a8c0
alecwbr <110862427+alecwbr@users.noreply.github.com>
2022-09-06 03:03:06 -0600

    fix test function typo

--- server/tests/integration/apiv2/authors/test_get_authors.py
+++ server/tests/integration/apiv2/authors/test_get_authors.py
@@ -9,7 +9,7 @@ def test_root_object_has_keys(client, endpoint):
         assert {'_links', 'count', 'authors'}.issubset(response.json)
 
 @pytest.mark.parametrize('endpoint', endpoints_list)
-def test_books_object_has_keys(client, endpoint):
+def test_authors_object_has_keys(client, endpoint):
     with client:
         response = client.get(endpoint)
         assert {'_links', 'id', 'name'}.issubset(response.json['authors'][0])
--- server/tests/integration/apiv2/clips/test_get_clips.py
+++ server/tests/integration/apiv2/clips/test_get_clips.py
@@ -9,7 +9,7 @@ def test_root_object_has_keys(client, endpoint):
         assert {'_links', 'count', 'clips'}.issubset(response.json)
 
 @pytest.mark.parametrize('endpoint', endpoints_list)
-def test_books_object_has_keys(client, endpoint):
+def test_clips_object_has_keys(client, endpoint):
     with client:
         response = client.get(endpoint)
         assert {'_links', 'id', 'clip_type', 'location', 'date', 'highlight', 'author_id', 'author_name', 'book_id', 'book_name'}.issubset(response.json['clips'][0])
