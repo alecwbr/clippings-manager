4ce030a315a270ff1648012e0d3e5048debfbf33
alecwbr <110862427+alecwbr@users.noreply.github.com>
2022-09-10 23:51:18 -0600

    add pagination to get_clip_tags

--- server/app/apiv2/tags.py
+++ server/app/apiv2/tags.py
@@ -1,7 +1,7 @@
 from . import apiv2
 from .. import db
 from ..models import Tag, Clip
-from flask import jsonify, url_for, request
+from flask import jsonify, url_for, request, current_app
 
 @apiv2.route('/tags')
 def get_tags():
@@ -25,16 +25,24 @@ def get_tag(tag_id):
 
 @apiv2.route('/clips/<int:clip_id>/tags')
 def get_clip_tags(clip_id):
-    clip = Clip.query.get(clip_id)
+    page = request.args.get('page', 1, type=int)
+    pagination = Clip.query.filter_by(id=clip_id).first().tags.paginate(
+        page, per_page=current_app.config['TAGS_PER_PAGE'], error_out=False
+    )
+    tags = pagination.items
+    prev_p = url_for('.get_clip_tags', _external=True, clip_id=clip_id, page=page-1) if pagination.has_prev else None
+    next_p = url_for('.get_clip_tags', _external=True, clip_id=clip_id, page=page+1) if pagination.has_next else None
+
     tags_list = []
-    for tag in clip.tags:
+    for tag in tags:
         tags_list.append(tag.to_json())
     json_res = {
         '_links': {
             'self': { 'href': url_for('.get_clip_tags', _external=True, clip_id=clip_id) },
-            'clip': { 'href': url_for('.get_clip', _external=True, clip_id=clip_id) }
+            'prev': { 'href': prev_p },
+            'next': { 'href': next_p }
         },
-        'count': len(tags_list),
+        'count': pagination.total,
         'tags': tags_list
     }
     return jsonify(json_res)
